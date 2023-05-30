055193516cca23664202f7991cd29561cdaa3160
Sean Gray <seangray.dev@gmail.com>
2023-05-30 12:59:40 -0400

    add temp to fetch data in packs page...in prog

--- frontend/app/packs/[id]/page.tsx
+++ frontend/app/packs/[id]/page.tsx
@@ -1,3 +1,46 @@
+'use client';
+
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+
+import Nav from '@/app/components/Layout/Nav';
+import AudioPlayer from '@/app/components/AudioPlayer';
+
+{
+  /* @ts-expect-error Async Server Component */
+}
 export default function Packs({ params }: { params: { id: string } }) {
-  return <h1>Pack ID: {params.id}</h1>;
+  const [pack, setPack] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          `http://your-django-api-url.com/packs/${params.id}`
+        );
+        setPack(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+  }, [params.id]);
+
+  return (
+    <>
+      <Nav />
+      <main className='flex flex-col md:grid grid-cols-2'>
+        <h1>Pack ID: {params.id}</h1>
+        {pack && (
+          <div>
+            <h2>{pack.name}</h2>
+            <p>{pack.description}</p>
+            {/* Display more pack data here */}
+          </div>
+        )}
+      </main>
+      <AudioPlayer />
+    </>
+  );
 }
