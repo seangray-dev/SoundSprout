51eed3c9e8638f1427b987fdf40ae2d1732619a9
Sean Gray <seangray.dev@gmail.com>
2023-06-05 23:08:02 -0400

    fetch audio files from cloudinary

--- frontend/app/components/Layout/PackSounds.tsx
+++ frontend/app/components/Layout/PackSounds.tsx
@@ -3,7 +3,13 @@ import React, { useState, useEffect } from 'react';
 const PackSounds = ({ packId }) => {
   const [sounds, setSounds] = useState([]);
 
-  useEffect(() => {
+  const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+
+  function getPreviewUrl(audio_file) {
+    return `https://res.cloudinary.com/${cloudName}/video/upload/packs${audio_file}`;
+  }
+
+  https: useEffect(() => {
     const fetchSounds = async () => {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/packs/${packId}/sounds`
@@ -15,7 +21,20 @@ const PackSounds = ({ packId }) => {
     fetchSounds();
   }, [packId]);
 
-  return (
+  console.log(
+    sounds.map((sound) => {
+      return (
+        `https://res.cloudinary.com/${cloudName}/video/upload/packs` +
+        sound.audio_file
+      );
+    })
+  );
+
+  // res.cloudinary.com/dfcg1eeap/video/upload/packs/Tape%20and%20Vinyl%20Drums/sounds/TVD_snare_tape_vinyl_pong.wav
+
+  //res.cloudinary.com/dfcg1eeap/video/upload/packs/Tape%20%26%20Vinyl%20Drums/sounds/TVD_snare_tape_vinyl_pong
+
+  https: return (
     <section className='container mt-10 mb-20'>
       <header className='border-b border-t border-black flex justify-between'>
         <div>Filename</div>
@@ -27,7 +46,7 @@ const PackSounds = ({ packId }) => {
         {sounds.map((sound, index) => (
           <li className='' key={index}>
             <div className='text-sm'>{sound.name}</div>
-            {/* <audio src={sound.audio_file} controls></audio> */}
+            <audio src={getPreviewUrl(sound.audio_file)} controls></audio>
           </li>
         ))}
       </ul>
