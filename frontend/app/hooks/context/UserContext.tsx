7528f160e3798cc1d14fefbc9f43a7edad179d00
Sean Gray <seangray.dev@gmail.com>
2023-07-24 15:08:18 -0400

    fix types errors

--- frontend/app/hooks/context/UserContext.tsx
+++ frontend/app/hooks/context/UserContext.tsx
@@ -1,15 +1,29 @@
 'use client';
 
-import React, { createContext, ReactNode } from 'react';
+import { User } from '@/app/types';
+import React, {
+	Dispatch,
+	ReactNode,
+	SetStateAction,
+	createContext,
+} from 'react';
 
-export const UserContext = createContext({});
+interface UserContextProps {
+	user: User | null;
+	setUser: Dispatch<SetStateAction<User | null>>;
+}
+
+export const UserContext = createContext<UserContextProps>({
+	user: null,
+	setUser: () => {},
+});
 
 interface UserProviderProps {
 	children: ReactNode;
 }
 
 export const UserProvider = ({ children }: UserProviderProps) => {
-	const [user, setUser] = React.useState(null);
+	const [user, setUser] = React.useState<User | null>(null);
 	console.log('Context User:', user);
 
 	return (
--- frontend/app/types.ts
+++ frontend/app/types.ts
@@ -3,6 +3,7 @@ export interface User {
 	first_name: string;
 	last_name: string;
 	email: string;
+	username: string;
 }
 
 export interface Pack {
