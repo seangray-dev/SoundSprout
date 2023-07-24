c2d98fbadc84506e0712c4dc96617472c8397d00
Sean Gray <seangray.dev@gmail.com>
2023-07-24 13:43:33 -0400

    add types.ts file to match models from backend

---
+++ frontend/app/types.ts
@@ -0,0 +1,63 @@
+export interface User {
+	id: number;
+	first_name: string;
+	last_name: string;
+	email: string;
+}
+
+export interface Pack {
+	id: number;
+	name: string;
+	description: string;
+	price: number;
+	cover_art_location: string;
+	uploader: User;
+	preview: string | null;
+	purchase_count: number;
+	created_at: string;
+	modified_at: string;
+}
+
+export interface Sound {
+	id: number;
+	name: string;
+	audio_file: string;
+	price: number;
+	pack: Pack;
+	purchase_count: number;
+	created_at: string;
+	modified_at: string;
+}
+
+export interface Transaction {
+	id: number;
+	buyer: User;
+	pack: Pack | null;
+	sound: Sound | null;
+	date: string;
+	total_price: number;
+	created_at: string;
+	modified_at: string;
+}
+
+export interface Genre {
+	id: number;
+	name: string;
+}
+
+export interface PackGenreAssociation {
+	id: number;
+	pack: Pack;
+	genre: Genre;
+}
+
+export interface Tag {
+	id: number;
+	name: string;
+}
+
+export interface SoundTagAssociation {
+	id: number;
+	sound: Sound;
+	tag: Tag;
+}
