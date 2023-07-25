03bb0e510a0041dc47f5690c5f8a2535cb49f4dd
Sean Gray <seangray.dev@gmail.com>
2023-07-24 21:54:36 -0400

    reformat signup page

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,34 +1,33 @@
 import Image from 'next/image';
-
-import Heading from '../components/Utils/Heading';
 import SignupForm from '../components/Forms/SignupForm';
+import Heading from '../components/Utils/Heading';
 
 const Signup = () => {
-  return (
-    <>
-      <main className='bg-gray-1'>
-        <section className='2xl:container mx-auto flex flex-col md:grid md: grid-cols-2'>
-          <Image
-            src={'/assets/images/login-signup-header.jpeg'}
-            width={1000}
-            height={1000}
-            alt='producer in studio'
-            className='mx-auto h-full object-cover object-center'
-          />
-          <div className='px-8 mt-8 mb-10'>
-            <Heading level={1} className='text-white text-4xl mb-5'>
-              Create Account
-            </Heading>
-            <p className='text-white'>
-              Welcome! Enter your details and start exploring, collecting and
-              selling samples.
-            </p>
-            <SignupForm />
-          </div>
-        </section>
-      </main>
-    </>
-  );
+	return (
+		<>
+			<main className='bg-gray-1'>
+				<section className='2xl:container mx-auto flex flex-col md:grid md: grid-cols-2'>
+					<Image
+						src={'/assets/images/login-signup-header.jpeg'}
+						width={1000}
+						height={1000}
+						alt='producer in studio'
+						className='mx-auto h-full object-cover object-center'
+					/>
+					<div className='px-8 mt-8 mb-10'>
+						<Heading level={1} className='text-white text-4xl mb-5'>
+							Create Account
+						</Heading>
+						<p className='text-white'>
+							Welcome! Enter your details and start exploring, collecting and
+							selling samples.
+						</p>
+						<SignupForm />
+					</div>
+				</section>
+			</main>
+		</>
+	);
 };
 
 export default Signup;
