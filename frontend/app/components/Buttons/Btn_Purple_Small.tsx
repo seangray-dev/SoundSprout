93ce466448c697e6cdd6b9dcf5334716bcd363e9
Sean Gray <seangray.dev@gmail.com>
2023-07-25 13:40:30 -0400

    add Btn_Purple_Small comp

---
+++ frontend/app/components/Buttons/Btn_Purple_Small.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react';
+
+interface BtnPrimaryProps {
+	children?: ReactNode;
+	type?: 'button' | 'submit' | 'reset';
+}
+
+const Btn_Primary_Small = ({ children, type = 'button' }: BtnPrimaryProps) => {
+	return (
+		<button
+			type={type}
+			className='flex justify-center items-center py-1 px-4 gap-1 bg-purple rounded-full text-white hover:opacity-70 transition-opacity duration-300'>
+			{children}
+		</button>
+	);
+};
+
+export default Btn_Primary_Small;
