332ae6ad4417b577d1939029efc9285fdfd3c394
Sean Gray <seangray.dev@gmail.com>
2023-07-26 10:34:00 -0400

    add buttons to sort sounds by file, time, key & bpm

--- frontend/app/components/Layout/PackSounds.tsx
+++ frontend/app/components/Layout/PackSounds.tsx
@@ -16,6 +16,10 @@ const PackSounds = ({ packId, coverArtLocation }: PackSoundsProps) => {
 	);
 	const audioRefs = useRef<HTMLAudioElement[]>([]);
 	const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+	const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+	const [sortField, setSortField] = useState<
+		'name' | 'duration' | 'key' | 'bpm'
+	>('name');
 
 	function getPreviewUrl(audio_file: string) {
 		return `${process.env.NEXT_PUBLIC_CLOUDINARY_SOUND_URL}${audio_file}`;
@@ -55,27 +59,73 @@ const PackSounds = ({ packId, coverArtLocation }: PackSoundsProps) => {
 			.padStart(2, '0')}`;
 	};
 
+	// Function to sort the sounds
+	const handleSort = (field: 'name' | 'duration' | 'key' | 'bpm') => {
+		if (field !== sortField) {
+			setSortField(field);
+			setSortOrder('asc');
+		} else {
+			setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+		}
+
+		const sortedSounds = [...sounds].sort((a, b) => {
+			let compareA: number | string, compareB: number | string;
+
+			switch (field) {
+				case 'name':
+				case 'key':
+					compareA = a[field];
+					compareB = b[field];
+					break;
+				case 'duration':
+					compareA = durations[a.id];
+					compareB = durations[b.id];
+					break;
+				case 'bpm':
+					compareA = a[field];
+					compareB = b[field];
+					break;
+			}
+
+			if (sortOrder === 'asc') {
+				return compareA < compareB ? -1 : 1;
+			} else {
+				return compareA > compareB ? -1 : 1;
+			}
+		});
+
+		setSounds(sortedSounds);
+	};
+
 	return (
 		<section className='container mt-10 mb-20 font-light'>
 			<header className='border-t border-b text-sm text-gray-500 grid grid-cols-[1fr_3fr_1fr_1fr_1fr] gap-2 py-2'>
 				<div className='ml-10'>Pack</div>
 				<div>
-					<button className='flex items-center'>
+					<button
+						className='flex items-center'
+						onClick={() => handleSort('name')}>
 						Filename <ChevronUpDownIcon className='w-4 h-4' />
 					</button>
 				</div>
 				<div>
-					<button className='flex items-center'>
+					<button
+						className='flex items-center'
+						onClick={() => handleSort('duration')}>
 						Time <ChevronUpDownIcon className='w-4 h-4' />
 					</button>
 				</div>
 				<div>
-					<button className='flex items-center'>
+					<button
+						className='flex items-center'
+						onClick={() => handleSort('key')}>
 						Key <ChevronUpDownIcon className='w-4 h-4' />
 					</button>
 				</div>
 				<div>
-					<button className='flex items-center'>
+					<button
+						className='flex items-center -ml-3'
+						onClick={() => handleSort('bpm')}>
 						BPM <ChevronUpDownIcon className='w-4 h-4' />
 					</button>
 				</div>
@@ -126,7 +176,7 @@ const PackSounds = ({ packId, coverArtLocation }: PackSoundsProps) => {
 								{sound.name}
 							</div>
 						</div>
-						{/* <audio
+						<audio
 							// controls
 							ref={(el) => {
 								if (el) {
@@ -134,7 +184,7 @@ const PackSounds = ({ packId, coverArtLocation }: PackSoundsProps) => {
 								}
 							}}
 							src={getPreviewUrl(sound.audio_file)}
-							onLoadedMetadata={() => handleLoadedMetadata(index)}></audio> */}
+							onLoadedMetadata={() => handleLoadedMetadata(index)}></audio>
 
 						<div className='text-gray-500'>{formatTime(durations[index])}</div>
 						<div className='text-gray-500'>{sound.key}</div>
