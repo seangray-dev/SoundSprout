7ca0eb4207860d820c99ff8e66ad15aed5233c8b
Sean Gray <seangray.dev@gmail.com>
2023-07-27 15:50:50 -0400

    remove localStorage

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -2,33 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth-slice';
 import sortSounds from './features/sortSounds';
 
-let preloadedState;
-const persistedState = localStorage.getItem('authState');
-
-if (persistedState) {
-	preloadedState = {
-		authReducer: JSON.parse(persistedState),
-	};
-} else {
-	preloadedState = {
-		authReducer: {
-			isAuth: false,
-			user: null,
-		},
-	};
-}
-
 export const store = configureStore({
 	reducer: {
 		authReducer,
 	},
-	preloadedState,
-});
-
-store.subscribe(() => {
-	// When the store changes, save the auth state to localStorage.
-	const state = store.getState();
-	localStorage.setItem('authState', JSON.stringify(state.authReducer));
 });
 
 export type RootState = ReturnType<typeof store.getState>;
