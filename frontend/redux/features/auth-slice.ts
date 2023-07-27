0490f32dae5acd9822d78c621d1cd8fe259a177a
Sean Gray <seangray.dev@gmail.com>
2023-07-27 14:45:24 -0400

    use localStorage for persisting user in state

--- frontend/redux/features/auth-slice.ts
+++ frontend/redux/features/auth-slice.ts
@@ -1,4 +1,5 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { fetchUser } from '@/app/api/api';
+import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 type User = {
 	username: string;
@@ -18,17 +19,38 @@ const initialState: AuthState = {
 	user: null,
 };
 
+export const fetchAuthUser = createAsyncThunk('auth/fetchUser', async () => {
+	try {
+		const data = await fetchUser();
+		return data.user;
+	} catch (error) {
+		throw new Error('Failed to fetch user');
+	}
+});
+
 export const auth = createSlice({
 	name: 'auth',
 	initialState,
 	reducers: {
 		logout: () => {
+			localStorage.removeItem('token');
 			return initialState;
 		},
 		login: (state, action: PayloadAction<User>) => {
 			state.isAuth = true;
 			state.user = action.payload;
 		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchAuthUser.fulfilled, (state, action) => {
+				state.isAuth = true;
+				state.user = action.payload;
+			})
+			.addCase(fetchAuthUser.rejected, (state, action) => {
+				state.isAuth = false;
+				state.user = null;
+			});
 	},
 });
 
