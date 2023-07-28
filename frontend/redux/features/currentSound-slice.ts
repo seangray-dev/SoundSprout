54c1a4248cd69d43d3586bf51b42e1bf500bd48a
Sean Gray <seangray.dev@gmail.com>
2023-07-28 19:30:51 -0400

    create currentSound slice

--- frontend/redux/features/currentSound-slice.ts
+++ frontend/redux/features/currentSound-slice.ts
@@ -3,11 +3,23 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 type CurrentSoundState = {
 	packId: string | null;
 	soundIndex: number | null;
+	coverArt: string | null;
+	name: string | null;
+	key: string | null;
+	bpm: number | null;
+	audioFile: string | null;
+	isPlaying: boolean;
 };
 
 const initialState: CurrentSoundState = {
 	packId: null,
 	soundIndex: null,
+	coverArt: null,
+	name: null,
+	key: null,
+	bpm: null,
+	audioFile: null,
+	isPlaying: false,
 };
 
 export const currentSound = createSlice({
@@ -17,14 +29,35 @@ export const currentSound = createSlice({
 		setCurrentSound: (state, action: PayloadAction<CurrentSoundState>) => {
 			state.packId = action.payload.packId;
 			state.soundIndex = action.payload.soundIndex;
+			state.coverArt = action.payload.coverArt;
+			state.name = action.payload.name;
+			state.key = action.payload.key;
+			state.bpm = action.payload.bpm;
+			state.audioFile = action.payload.audioFile;
 		},
 		resetCurrentSound: (state) => {
 			state.packId = null;
 			state.soundIndex = null;
+			state.coverArt = null;
+			state.name = null;
+			state.key = null;
+			state.bpm = null;
+			state.audioFile = null;
 		},
+		togglePlay: (state) => {
+			state.isPlaying = !state.isPlaying;
+		},
+		playNext: (state, action: PayloadAction<number>) => {},
+		playPrevious: (state, action: PayloadAction<number>) => {},
 	},
 });
 
-export const { setCurrentSound, resetCurrentSound } = currentSound.actions;
+export const {
+	setCurrentSound,
+	resetCurrentSound,
+	togglePlay,
+	playNext,
+	playPrevious,
+} = currentSound.actions;
 
 export default currentSound.reducer;
