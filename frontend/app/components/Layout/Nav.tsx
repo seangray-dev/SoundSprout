e4d347d79f8dfd38abdca442a8f6c3b2f6c6b523
Conlan Schafer <conlan.school@gmail.com>
2023-08-01 19:51:40 -0700

    Add routes and barebones page

--- frontend/app/components/Layout/Nav.tsx
+++ frontend/app/components/Layout/Nav.tsx
@@ -27,30 +27,30 @@ const Nav = () => {
 					<Image src={logo} width={150} alt='sound-sprout-logo' />
 				</Link>
 				<ul className='flex items-center gap-4 text-white'>
-          {isAuth ? (
-            <>
-              <li className='hover:underline transition-all'>
-                <Link href={'/upload'}>Upload</Link>
-              </li>
-              <li className='underline underline-offset-2 text-white flex gap-2 items-center'>
-                <Link href={'/profile'}>{user?.username}</Link>
-                <UserIcon className='text-purple w-6' />
-              </li>
-              <li>
-                <button onClick={onLogout} className='py-1 px-4 gap-4 bg-purple rounded-full hover:opacity-70 hover:cursor-pointer transition-opacity duration-300 w-full'>
-                  Logout
-                </button>
-              </li>
-            </>
-          ) : (
-            <li className='py-1 px-4 gap-4 bg-purple rounded-full hover:opacity-70 hover:cursor-pointer transition-opacity duration-300 w-full'>
-              <Link href={'/login'}>Login</Link>
-            </li>
-          )}
-        </ul>
-      </div>
-    </nav>
-  );
+					<li className='hover:underline transition-all'>
+						<Link href={'/upload'}>Upload</Link>
+					</li>
+					{isAuth ? (
+						<>
+							<li className='underline underline-offset-2 text-white flex gap-2 items-center'>
+								<Link href={'/profile'}>{user?.username}</Link>
+								<UserIcon className='text-purple w-6' />
+							</li>
+							<li>
+								<button onClick={onLogout} className='py-1 px-4 gap-4 bg-purple rounded-full hover:opacity-70 hover:cursor-pointer transition-opacity duration-300 w-full'>
+									Logout
+								</button>
+							</li>
+						</>
+					) : (
+						<li className='py-1 px-4 gap-4 bg-purple rounded-full hover:opacity-70 hover:cursor-pointer transition-opacity duration-300 w-full'>
+							<Link href={'/login'}>Login</Link>
+						</li>
+					)}
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Nav;
