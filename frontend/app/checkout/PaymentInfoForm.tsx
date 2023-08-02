c02de8a4d417f32e78ab7dffbc9acefec712bc48
Sean Gray <seangray.dev@gmail.com>
2023-08-02 12:40:01 -0400

    stripe integration

--- frontend/app/checkout/PaymentInfoForm.tsx
+++ frontend/app/checkout/PaymentInfoForm.tsx
@@ -1,97 +1,58 @@
-import { ErrorMessage, Field, Form, Formik } from 'formik';
-import * as Yup from 'yup';
+import {
+	PaymentElement,
+	useElements,
+	useStripe,
+} from '@stripe/react-stripe-js';
 import Heading from '../components/Utils/Heading';
 import { Button } from '../components/ui/button';
 
-const validationSchema = Yup.object({
-	cardName: Yup.string().required('Cardholder Name is required'),
-	cardNumber: Yup.string()
-		.required('Card Number is required')
-		.length(16, 'Card Number must be 16 characters long'),
-	expirationDate: Yup.string().required('Expiration Date is required'),
-	cvv: Yup.string()
-		.required('CVV is required')
-		.length(3, 'CVV must be 3 characters long'),
-});
-
 const PaymentInfoForm = () => {
+	const stripe = useStripe();
+	const elements = useElements();
+
+	const handleSubmit = async (event) => {
+		// We don't want to let default form submission happen here,
+		// which would refresh the page.
+		event.preventDefault();
+
+		if (!stripe || !elements) {
+			// Stripe.js hasn't yet loaded.
+			// Make sure to disable form submission until Stripe.js has loaded.
+			return;
+		}
+
+		const result = await stripe.confirmPayment({
+			//`Elements` instance that was used to create the Payment Element
+			elements,
+			confirmParams: {
+				return_url: 'https://example.com/order/123/complete',
+			},
+		});
+
+		if (result.error) {
+			// Show error to your customer (for example, payment details incomplete)
+			console.log(result.error.message);
+		} else {
+			// Your customer will be redirected to your `return_url`. For some payment
+			// methods like iDEAL, your customer will be redirected to an intermediate
+			// site first to authorize the payment, then redirected to the `return_url`.
+		}
+	};
+
 	return (
 		<section className='mt-6 border rounded-md p-4'>
 			<Heading level={2} className='text-2xl mb-6'>
 				Payment Info
 			</Heading>
-			<Formik
-				initialValues={{
-					cardName: '',
-					cardNumber: '',
-					expirationDate: '',
-					cvv: '',
-				}}
-				validationSchema={validationSchema}
-				onSubmit={(values, { setSubmitting }) => {
-					console.log(values);
-					setSubmitting(false);
-				}}>
-				<Form className='flex flex-col gap-4'>
-					<div className='relative flex flex-col gap-1'>
-						<label htmlFor='cardName'>Cardholder Name</label>
-						<Field
-							className='border rounded-md outline-none focus:border-purple p-2 transition-all duration-300'
-							name='cardName'
-							type='text'
-						/>
-						<ErrorMessage
-							className='text-red-500 text-sm absolute bottom-3 right-2'
-							name='cardName'
-							component='div'
-						/>
-					</div>
-					<div className='relative flex flex-col gap-1'>
-						<label htmlFor='cardNumber'>Card Number</label>
-						<Field
-							className='border rounded-md outline-none focus:border-purple p-2 transition-all duration-300'
-							name='cardNumber'
-							type='text'
-						/>
-						<ErrorMessage
-							className='text-red-500 text-sm absolute bottom-3 right-2'
-							name='cardNumber'
-							component='div'
-						/>
-					</div>
-					<div className='relative flex flex-col gap-1'>
-						<label htmlFor='expirationDate'>Expiration Date (MM/YY)</label>
-						<Field
-							className='border rounded-md outline-none focus:border-purple p-2 transition-all duration-300'
-							name='expirationDate'
-							type='text'
-						/>
-						<ErrorMessage
-							className='text-red-500 text-sm absolute bottom-3 right-2'
-							name='expirationDate'
-							component='div'
-						/>
-					</div>
-					<div className='relative flex flex-col gap-1'>
-						<label htmlFor='cvv'>CVV</label>
-						<Field
-							className='border rounded-md outline-none focus:border-purple p-2 transition-all duration-300'
-							name='cvv'
-							type='text'
-						/>
-						<ErrorMessage
-							className='text-red-500 text-sm absolute bottom-3 right-2'
-							name='cvv'
-							component='div'
-						/>
-					</div>
-					<Button
-						className='bg-purple border border-purple py-2 text-base hover:bg-white hover:text-purple'
-						type='submit'>
-						Submit Payment
-					</Button>
-				</Form>
-			</Formik>
+			<form onSubmit={handleSubmit}>
+				<PaymentElement />
+				<Button
+					className='w-full py-2 mt-4 bg-purple border border-purple
+					hover:text-purple hover:bg-transparent transition-all duration-300'
+					disabled={!stripe}>
+					Submit
+				</Button>
+			</form>
 		</section>
 	);
 };
--- frontend/app/checkout/page.tsx
+++ frontend/app/checkout/page.tsx
@@ -1,7 +1,11 @@
 'use client';
 
 import { RootState } from '@/redux/store';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import axios from 'axios';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { getCoverArtUrl } from '../api/cloudinary';
 import Heading from '../components/Utils/Heading';
@@ -11,6 +15,10 @@ import { CartItem, columns } from './columns';
 import { DataTable } from './data-table';
 
 const Checkout = () => {
+	const [clientSecret, setClientSecret] = useState('');
+	const stripePromise = loadStripe(
+		`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`
+	);
 	const cartItems = useSelector((state: RootState) => state.cartReducer.items);
 	const subtotal = cartItems.reduce(
 		(total, item) => total + Number(item.price),
@@ -19,6 +27,23 @@ const Checkout = () => {
 	const HST_RATE = 0.13;
 	const hst = subtotal * HST_RATE;
 	const total = subtotal + hst;
+
+	useEffect(() => {
+		axios
+			.post(
+				`${process.env.NEXT_PUBLIC_BACKEND_SERVER}/create-payment-intent/`,
+				{
+					amount: total,
+				}
+			)
+			.then((res) => {
+				console.log(res.data.clientSecret);
+				setClientSecret(res.data.clientSecret);
+			})
+			.catch((err) => {
+				console.error('Checkout: Error fetching client secret', err);
+			});
+	}, []);
 
 	return (
 		<section className='container my-6'>
@@ -29,11 +54,15 @@ const Checkout = () => {
 				<p>Your cart is empty!</p>
 			) : (
 				<>
-					<DataTable columns={columns} data={cartItems} />
-					<div className='grid grid-cols-2 gap-6'>
-						<PaymentInfoForm />
-						<Summary subtotal={subtotal} hst={hst} total={total} />
-					</div>
+					{clientSecret && (
+						<Elements stripe={stripePromise} options={{ clientSecret }}>
+							<DataTable columns={columns} data={cartItems} />
+							<div className='grid grid-cols-2 gap-6'>
+								<PaymentInfoForm />
+								<Summary subtotal={subtotal} hst={hst} total={total} />
+							</div>
+						</Elements>
+					)}
 				</>
 			)}
 		</section>
