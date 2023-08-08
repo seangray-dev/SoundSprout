ac4516fb888e2cf9ca6b17becc06a6da4390e529
Sean Gray <seangray.dev@gmail.com>
2023-08-07 20:23:24 -0400

    add hover effect to genre cards

--- frontend/app/components/Layout/GenreCard.tsx
+++ frontend/app/components/Layout/GenreCard.tsx
@@ -17,8 +17,14 @@ const sanitizePath = (path: string) => {
 const GenreCard: React.FC<GenreCardProps> = ({ imagePath, genre, id }) => {
 	return (
 		<Link href={`/genres/${id}`}>
-			<li className='list-none rounded-2xl overflow-hidden hover:cursor-pointer'>
-				<Image width={360} height={240} src={imagePath} alt={genre} />
+			<li className='list-none rounded-2xl overflow-hidden hover:cursor-pointer transform transition-transform hover:scale-105 duration-300'>
+				<Image
+					className='object-cover'
+					width={360}
+					height={240}
+					src={imagePath}
+					alt={genre}
+				/>
 				<div className='bg-gray-2 text-xs font-bold text-white tracking-wide text-center px-5 py-5'>
 					<p>{genre}</p>
 				</div>
