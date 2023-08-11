7a3a7e4b9dd5d5cf6fb539fe5094ada1fc186ca2
Sean Gray <seangray.dev@gmail.com>
2023-08-11 10:29:29 -0400

    add hover effect to howitworksitem icons

--- frontend/app/components/Layout/HowItWorksItem.tsx
+++ frontend/app/components/Layout/HowItWorksItem.tsx
@@ -10,8 +10,8 @@ const HowItWorksItem: React.FC<HowItWorksItemProps> = ({
 	description,
 }) => {
 	return (
-		<li className='bg-gray-2 rounded-2xl p-8 text-white'>
-			{icon}
+		<li className='bg-gray-2 rounded-2xl p-8 text-white how-it-works-item transition-transform duration-200'>
+			<div className='icon-container'>{icon}</div>
 			<h6 className='text-center font-bold mt-5 mb-2'>{title}</h6>
 			<p className='text-center'>{description}</p>
 		</li>
