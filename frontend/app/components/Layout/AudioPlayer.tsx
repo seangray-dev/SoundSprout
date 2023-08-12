4361bf239a6ce5726761249c42bfe7a6e91b13d2
Sean Gray <seangray.dev@gmail.com>
2023-08-12 17:29:36 -0400

    ui changes to audio player component

--- frontend/app/components/Layout/AudioPlayer.tsx
+++ frontend/app/components/Layout/AudioPlayer.tsx
@@ -49,7 +49,6 @@ const AudioPlayer: React.FC = (props) => {
 			audioRef.current.currentTime = 0;
 			if (currentSound.soundIndex !== null) {
 				audioRef.current.src = currentSound.audio_file as string;
-				audioRef.current.loop = true;
 				audioRef.current.play();
 				audioRef.current.ontimeupdate = () => {
 					setProgress(
@@ -82,14 +81,26 @@ const AudioPlayer: React.FC = (props) => {
 		}
 	};
 
+	const handleSliderChange = (sliderValues: number[]) => {
+		const sliderValue = sliderValues[0];
+		if (audioRef.current) {
+			audioRef.current.currentTime =
+				(sliderValue / 100) * audioRef.current.duration;
+		}
+	};
+
 	return (
 		<div className='fixed inset-x-0 bottom-0 bg-gray-1 text-white'>
-			<Progress className='w-full bg-transparent rounded-none' value={progress}>
-				{' '}
-			</Progress>
-			<div className='container grid grid-cols-2 my-8'>
+			<Slider
+				className='hover:cursor-pointer'
+				value={[progress]}
+				max={100}
+				step={0.1}
+				onValueChange={handleSliderChange}
+			/>
+			<div className='container grid grid-cols-3'>
 				<audio ref={audioRef} />
-				{/* <div className='flex items-center gap-2 my-8'>
+				<div className='flex items-center gap-2 my-8'>
 					<BackwardIcon
 						className='w-6 h-6 hover:cursor-pointer hover:text-purple transition-all duration-300'
 						onClick={() => dispatch(playPrevious)}></BackwardIcon>
@@ -101,7 +112,7 @@ const AudioPlayer: React.FC = (props) => {
 							/>
 						) : (
 							<PlayIcon
-								className='w-7 h-7 hover:cursor-pointer hover:text-purple transition-all duration-300'
+								className='w-6 h-6 hover:cursor-pointer hover:text-purple transition-all duration-300'
 								onClick={() => dispatch(togglePlay())}
 							/>
 						)}
@@ -109,30 +120,30 @@ const AudioPlayer: React.FC = (props) => {
 					<ForwardIcon
 						className='w-6 h-6 hover:cursor-pointer hover:text-purple transition-all duration-300'
 						onClick={() => dispatch(playNext)}></ForwardIcon>
-				</div> */}
-				<div className='grid grid-cols-2 gap-2 items-center'>
-					<div className='flex gap-4 items-center'>
+				</div>
+				<div className='grid grid-cols-[55%,_45%] gap-2 items-center '>
+					<div className='flex gap-4 items-center border-l border-gray-2 h-full pl-6'>
 						<img
 							className='w-12 h-12'
 							src={
 								currentSound.coverArt || '/assets/images/default-audiofile.jpeg'
 							}
-							alt=''
+							alt={currentSound.name}
 						/>
 						<div className='flex flex-col overflow-scroll marquee'>
 							<span>{currentSound.name}</span>
 						</div>
 					</div>
-					<div className='flex gap-4 justify-between items-center'>
-						<div className='flex flex-col items-center'>
-							<span>{currentSound.key || '--'}</span>
-							<span className='text-sm'>KEY</span>
+					<div className='grid grid-cols-3 items-center h-full'>
+						<div className='flex flex-col gap-2 justify-center items-center border-x border-gray-2 h-full whitespace-nowrap'>
+							<span className='text-sm'>{currentSound.key || '--'}</span>
+							<span className='text-xs'>KEY</span>
 						</div>
-						<div className='flex flex-col items-center'>
-							<span>{currentSound.bpm || '--'}</span>
-							<span className='text-sm'>BPM</span>
+						<div className='flex flex-col gap-2 justify-center items-center border-r border-gray-2 h-full'>
+							<span className='text-sm'>{currentSound.bpm || '--'}</span>
+							<span className='text-xs'>BPM</span>
 						</div>
-						<div className='flex items-center'>
+						<div className='flex justify-center border-r border-gray-2 h-full'>
 							<button onClick={handleAddToCartClick}>
 								<PlusIcon className='w-6 h-6 hover:cursor-pointer hover:bg-purple transition-all duration-300 rounded-md'></PlusIcon>
 							</button>
